a52d8919607d8892f16f8cdad235b406927311aa
Carl <99614187+carl-baby@users.noreply.github.com>
2022-11-11 18:05:06 -0600

    part one complete

--- queries.sql
+++ queries.sql
@@ -1,5 +1,10 @@
 -- Part 1: Test it with SQL
-
+--CREATE TABLE job (
+--   column name job_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+--   column name job_name VARCHAR(255),
+--   column name employer VARCHAR(255),
+--   column name skills VARCHAR(255)
+--);
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
